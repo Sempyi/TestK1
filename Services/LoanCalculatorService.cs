a7c7b518fe1b01be56c579e96babf3282af1829f
Sempyi <167787212+Sempyi@users.noreply.github.com>
2024-10-20 20:35:47 +0500

    Add files via upload

--- Services/LoanCalculatorService.cs
+++ Services/LoanCalculatorService.cs
@@ -5,15 +5,21 @@ using TestK1.Models;
 
 namespace LoanCalculator.Services
 {
+    public enum PaymentStepType
+    {
+        Days,
+        Months
+    }
+
     public class LoanCalculatorService : ILoanCalculatorService
     {
         public LoanResultModel CalculateLoanInDays(LoanDailyInputModel dailyInputModel)
         {
-            decimal monthlyInterestRate = dailyInputModel.InterestRate * dailyInputModel.PaymentStep / 100;
+            decimal dailyInterestRate = dailyInputModel.InterestRate * dailyInputModel.PaymentStep / 100;
             int term = dailyInputModel.LoanTerm / dailyInputModel.PaymentStep;
             decimal loanAmount = dailyInputModel.LoanAmount;
 
-            return CalculateLoan(loanAmount, term, monthlyInterestRate);
+            return CalculateLoan(loanAmount, term, dailyInterestRate, PaymentStepType.Days, dailyInputModel.PaymentStep);
         }
 
         public LoanResultModel CalculateLoan(Models.LoanInputModel inputModel)
@@ -22,38 +28,48 @@ namespace LoanCalculator.Services
             int term = inputModel.LoanTerm;
             decimal loanAmount = inputModel.LoanAmount;
 
-            return CalculateLoan(loanAmount, term, monthlyInterestRate);
+            return CalculateLoan(loanAmount, term, monthlyInterestRate, PaymentStepType.Months, 1);
         }
 
-        public LoanResultModel CalculateLoan(decimal loanAmount, int term, decimal monthlyInterestRate)
+        public LoanResultModel CalculateLoan(decimal loanAmount, int term, decimal interestRate, PaymentStepType stepType, int paymentStep)
         {
-            decimal monthlyPayment = loanAmount * monthlyInterestRate / (1 - (decimal)Math.Pow(1 + (double)monthlyInterestRate, -term));
+            decimal payment = loanAmount * interestRate / (1 - (decimal)Math.Pow(1 + (double)interestRate, -term));
 
             var result = new LoanResultModel
             {
-                MonthlyPayment = monthlyPayment,
-                Schedule = GenerateMonthlyPaymentSchedule(loanAmount, term, monthlyInterestRate, monthlyPayment)
+                MonthlyPayment = payment,
+                Schedule = GeneratePaymentSchedule(loanAmount, term, interestRate, payment, stepType, paymentStep)
             };
 
             result.TotalOverpayment = result.Schedule.Sum(s => s.InterestPayment);
             return result;
         }
 
-        private List<PaymentScheduleItem> GenerateMonthlyPaymentSchedule(decimal loanAmount, int loanTerm, decimal monthlyInterestRate, decimal monthlyPayment)
+        private List<PaymentScheduleItem> GeneratePaymentSchedule(decimal loanAmount, int loanTerm, decimal interestRate, decimal payment, PaymentStepType stepType, int paymentStep)
         {
             var schedule = new List<PaymentScheduleItem>();
             decimal remainingBalance = loanAmount;
 
             for (int i = 1; i <= loanTerm; i++)
             {
-                decimal interestPayment = remainingBalance * monthlyInterestRate;
-                decimal principalPayment = monthlyPayment - interestPayment;
+                decimal interestPayment = remainingBalance * interestRate;
+                decimal principalPayment = payment - interestPayment;
                 remainingBalance -= principalPayment;
+
+                DateTime paymentDate;
+                if (stepType == PaymentStepType.Days)
+                {
+                    paymentDate = DateTime.Now.AddDays(i * paymentStep);
+                }
+                else
+                {
+                    paymentDate = DateTime.Now.AddMonths(i * paymentStep);
+                }
 
                 schedule.Add(new PaymentScheduleItem
                 {
                     PaymentNumber = i,
-                    PaymentDate = DateTime.Now.AddMonths(i),
+                    PaymentDate = paymentDate,
                     PrincipalPayment = principalPayment,
                     InterestPayment = interestPayment,
                     RemainingBalance = remainingBalance
